1b32afe649bd6413f956c58754e4b6706cf8d6a3
cnelmortimer <cnelmortimer@users.noreply.github.com>
2015-11-09 18:38:20 +0100

    Updated to the new result type

--- UsignPicture2ASCII.java
+++ UsignPicture2ASCII.java
@@ -1,6 +1,8 @@
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 import javax.imageio.ImageIO;
@@ -19,9 +21,21 @@ public class UsignPicture2ASCII {
 		try {
 			BufferedImage image = ImageIO.read(new File(scanner.next()));
 			Picture2ASCII conversor = new Picture2ASCII();
-			Picture2ASCIIResult resultado = conversor.convertPicture2ASCII(image, false, 1);//Not compressing: Sie of the window will be ignored
-			//Picture2ASCIIResult resultado = conversor.convertPicture2ASCII(image, true, 3);//Compressing
-			System.out.println(resultado.getResultData());
+			//Picture2ASCIIResult resultado = conversor.convertPicture2ASCII(image, false, 1);//Not compressing: Sie of the window will be ignored
+			Picture2ASCIIResult resultado = conversor.convertPicture2ASCII(image, true, 3);//Compressing
+			//System.out.println(resultado.getResultData());
+			if(resultado.getResultCode()==1){
+				FileWriter output = new FileWriter("output.txt");
+				PrintWriter pw = new PrintWriter(output);
+				String[] result = resultado.getResultData();
+				for(int i = 0; i<result.length; i++){
+					pw.print(result[i]);
+				}
+				output.close();
+				System.out.println("Done!");
+			}else{
+				System.out.println("Unexpected error...");
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
